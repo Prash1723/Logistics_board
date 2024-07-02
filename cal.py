3eb9e708ce6e96becce3c57b097ad8dc81e48c32
Prash1723 <s.praveen795@gmail.com>
2024-07-02 20:25:47 +0530

    company_name

--- cal.py
+++ cal.py
@@ -11,6 +11,10 @@ class kpi:
         self.n = n
         self.s = s
 
+    def company(self):
+        name = input("Type your Company name? : ")
+        return name
+
     def cbm(self):
     	return self.l*self.b*self.h
 
@@ -20,5 +24,6 @@ class kpi:
     def run(self):
         CBM = self.cbm()
         Space = self.space_left()
+        rc.print("Company :", self.company(), style="yellow")
         rc.print("CBM of load: ", CBM, style="green")
         rc.print("Space taken: ", Space, style="red")
