cc087e9e47d08ed3e8918b4901ec6d1e497f7fa2
Prash1723 <s.praveen795@gmail.com>
2024-07-02 20:31:28 +0530

    Code comments

--- cal.py
+++ cal.py
@@ -5,20 +5,23 @@ rc = Console()
 
 class kpi:
     def __init__(self, l, b, h, n, s):
-        self.l = l
-        self.b = b
-        self.h = h
-        self.n = n
-        self.s = s
+        self.l = l                                          #Length
+        self.b = b                                          #Breadth
+        self.h = h                                          #Height
+        self.n = n                                          #Number of units
+        self.s = s                                          #Space available
 
     def company(self):
+        """Assign company name"""
         name = input("Type your Company name? : ")
         return name
 
     def cbm(self):
+        """Calculate CBM for the items"""
     	return self.l*self.b*self.h
 
     def space_left(self):
+        """Space left in the godown"""
         return self.s - self.cbm()*self.n
     
     def run(self):
