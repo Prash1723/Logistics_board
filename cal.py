76748aa083e52dda210af892aac2b1969d162e57
praveen <s.praveen795@gmail.com>
2025-04-27 23:51:45 +0530

    update\ otd

--- cal.py
+++ cal.py
@@ -4,12 +4,14 @@ from rich.console import Console
 rc = Console()
 
 class kpi:
-    def __init__(self, l, b, h, n, s):
+    def __init__(self, l, b, h, n, s, OD, TD):
         self.l = l                                          #Length
         self.b = b                                          #Breadth
         self.h = h                                          #Height
         self.n = n                                          #Number of units
         self.s = s                                          #Space available
+        self.OD = OD                                        #Ontime Deliveries
+        self.TD = TD                                        #Total Deliveries
 
     def company(self):
         """Assign company name"""
@@ -18,23 +20,32 @@ class kpi:
 
     def cbm(self):
         """Calculate CBM for the items"""
-    	return self.l*self.b*self.h
+    	cbm = self.l * self.b * self.h
+        return cbm
+
+    def otd(self):
+        """Calculate Ontime Deliveries"""
+        otd = (self.OD/self.TD)*100
+        return otd
 
     def space_left(self):
         """Space left in the godown"""
-        return self.s - self.cbm()*self.n
+        space_left = self.s - self.cbm()*self.n
+        return space_left
 
-    def assortment(self):
-        question = input("Are there more than one companies in the list?")
-        if question == "yes"
+#    def assortment(self):
+#        question = input("Are there more than one companies in the list?")
+#        if question == "yes"
 
     def run(self):
         x = dict()
         CBM = self.cbm()
         Space = self.space_left()
+        OTD = self.otd()
         question = input("Are there more than one companies in the list?")
         if question == "yes":
             self.company()
         rc.print("Company :", self.company(), style="yellow")
         rc.print("CBM of load: ", CBM, style="green")
         rc.print("Space taken: ", Space, style="red")
+        rc.print("OTd %; ", OTD, style="orange")
